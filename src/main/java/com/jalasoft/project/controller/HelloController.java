0f863b4bffcf161a53c222b36346c4922f9a217a
Diego Garcia <qtimpot@gmail.com>
2020-07-09 20:17:10 -0400

    Add file param and save it in the home folder

--- src/main/java/com/jalasoft/project/controller/HelloController.java
+++ src/main/java/com/jalasoft/project/controller/HelloController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author HP
@@ -13,9 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    private Path filePath = buildFilePath("file");
+
     @PostMapping
     public String sayHello(@RequestParam(value = "name") String name,
-                           @RequestParam(value = "lastName") String lastName) {
+                           @RequestParam(value = "lastName") String lastName,
+                           @RequestParam("file") MultipartFile file) {
+        try {
+            file.transferTo(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "Hello " + name + " " + lastName;
+    }
+
+    private Path buildFilePath(String name) {
+        String home = System.getProperty("user.home");
+        return Paths.get(home, name + ".txt");
     }
 }
